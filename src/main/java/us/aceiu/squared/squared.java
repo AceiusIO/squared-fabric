1ed25e12324d84087902ec23c01bdcf655d1b344
Aceius <io@acei.us>
2021-07-24 20:40:00 -0700

    I too print

--- src/main/java/us/aceiu/squared/squared.java
+++ src/main/java/us/aceiu/squared/squared.java
@@ -2,9 +2,11 @@ package us.acei.squared;
 
 import java.util.Arrays;
 import java.util.function.Consumer;
+import net.fabricmc.api.ModInitializer;
 
-public class squared implements ClientModInitializer {
-	public void onInitializeClient() {
+public class squared implements ModInitializer {
+	@Override
+	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
