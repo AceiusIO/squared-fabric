6f105c677aeab3f58e6572ee05a82b019620cb38
Aceius <io@acei.us>
2021-07-24 23:24:46 -0700

    Revert "g"
    This reverts commit a16618c7d9fd3417ead8e10c7a9451ec7fe55d2d.

--- src/main/kotlin/com/mod_author/mod_id/ModName.kt
+++
@@ -1,9 +0,0 @@
-package us.acei.squared
-import net.fabricmc.api.ModInitializer
-@Suppress("UNUSED")
-object ModName: ModInitializer {
-    private const val MOD_ID = "mod_id"
-    override fun onInitialize() {
-        println("Example mod has been initialized.")
-    }
-}
